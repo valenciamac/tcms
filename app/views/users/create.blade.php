6fb5243ec21a9b890241bb2986d93ddb4d008e48
valenciamac <mac01218@yahoo.com>
2014-06-24 00:21:39 +0800

    create user fix

--- app/views/users/create.blade.php
+++ app/views/users/create.blade.php
@@ -44,60 +44,18 @@
     <div class="container">
         <a href="sysAdmin" class="brand"><img src="{{asset('img/brand1.png')}}" class="logo"></a>
         <div class="row">
-
-<div id="login-container">
-
-	<div id="login" class="row">
-		<div class="container" style="color:#1f1f1f;">
-		{{Form::open(array('route' => 'users.store','class'=>'form-horizontal','role'=>'form'))}}
-
-			<div class="form-group" style="padding-top:40px;">
-				<label for="fname" class="col-sm-3 control-label">First Name:&nbsp;</label>
-				<div class="col-sm-9">
-				<input name="fname" type="text" class="form-control" id="fname">
-			</div>
-			</div>
-			<div class="form-group">
-				<label for="lname" class="col-sm-3 control-label">Last Name:&nbsp;</label>
-				<div class="col-sm-9">
-				<input name="lname" type="text" class="form-control" id="lname">
-			</div>
-			</div>
-			<div class="form-group">
-				<label for="username" class="col-sm-3 control-label">Username:&nbsp;</label>
-				<div class="col-sm-9">
-				<input name="username" type="text" class="form-control" id="username">
-			</div>
-			</div>
-			<div class="form-group">
-				<label for="password" class="col-sm-3 control-label">Password:&nbsp;</label>
-				<div class="col-sm-9">
-				<input name="password" type="password" class="form-control" id="password">
-			</div>
-			</div>
-			<div class="form-group">
-				<label for="role" class="col-sm-3 control-label">role:&nbsp;</label>
-				<div class="col-sm-9">
-				<select name="role" class="form-control" id="role">
-					<option value="sysAdmin">System Administrator</option>
-					<option value="accounting">Accounting</option>
-					<option value="admin">Administrator</option>
-					<option value="financing">Financing</option>
-					<option value="purchasing">Purchasing</option>
-				</select>
-			</div>
-			</div>
-			<div class="form-group">
-
-				<button type="submit" id="login-btn"  class="btn btn-primary btn-block pull-right" style="width:90%;">Sign in &nbsp; <i class="fa fa-play-circle"></i></button>
-
-			</div>
-		{{Form::close()}}
-		</div>
-	</div> <!-- /#login -->
-
-
-</div> <!-- /#login-container -->
+<div class="col-md-3">
+    <ul class="sideMenu">
+        <li class="active">
+            <a href="sysAdmin"><i class="fa fa-home"></i>Dashboard</a>
+        </li>
+        <li>
+            <a href="accounts"><i class="fa fa-user"></i>Accounts</a>
+        </li>
+        <li>
+            <a href="#"><i class="fa fa-archive"></i>Inventory</a>
+        </li>
+    </ul>
 </div>
-</div>
+@include('users.sysAdmin.adminLayouts.createform')
 @include('users.includes.footer')
